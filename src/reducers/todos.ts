71fcf274f2b62fb65554b5e1b25f68ff5d3a9214
Oleh <orenkole@gmail.com>
2021-06-09 20:09:34 +0300

    275. Type Guards in Reducers

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,13 +1,14 @@
-import { Action } from "redux";
-import { Todo, FetchTodosAction, ActionTypes } from "../actions";
+import { Todo, Action, ActionTypes } from "../actions";
 
 export const todosReducer = (
   state: Todo[] = [],
-  action: FetchTodosAction // not just this action
+  action: Action
 ) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
