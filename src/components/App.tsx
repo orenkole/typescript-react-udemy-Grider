850dad3aea1bbcf9ba5b81c1c402e6d50c76fa35
Oleh <orenkole@gmail.com>
2021-06-09 20:19:17 +0300

    fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Todo, fetchTodos } from "../actions";
 import { StoreState } from "../reducers";
 import { Todo, fetchTodos, deleteTodo } from '../actions';
 
