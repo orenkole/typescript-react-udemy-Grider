c113f22dc0d1323e3b5f0a7fd7b839a6d424e58a
Oleh <orenkole@gmail.com>
2021-06-09 19:56:46 +0300

    273. Breaking Out Action Creators

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,6 +1,5 @@
 import { Action } from "redux";
-import { Todo, FetchTodosAction } from "../actions";
-import { ActionTypes } from "../actions/types";
+import { Todo, FetchTodosAction, ActionTypes } from "../actions";
 
 export const todosReducer = (
   state: Todo[] = [],
