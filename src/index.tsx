806754f3498a247d9515db0da95807129ac9fea1
Oleh <orenkole@gmail.com>
2021-06-09 16:57:11 +0300

    262. Confusing Component State! using interface AppState and this.state

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,31 @@ interface AppProps {
   color?: string
 }
 
-class App extends React.Component<AppProps> {
-  state = {counter: 0};
+interface AppState {
+  counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+  state = {counter: 0}
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {counter: 0};
+  }
+
+  onIncrement = (): void => {
+    this.setState({counter: this.state.counter + 1})
+  }
+  onDecrement = (): void => {
+    this.setState({counter: this.state.counter - 1})
+  }
+
   render() {
     return (
       <div>
-        Hi there
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
       </div>
     )
   }
