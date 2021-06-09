d2910f7f302b4631307bd82a2c7f85e9ddbb6e5e
Oleh <orenkole@gmail.com>
2021-06-09 19:43:11 +0300

    271. Rendering a List

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,10 +9,28 @@ interface AppProps {
 }
 
 export class _App extends React.Component<AppProps> {
+
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  }
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return (
+        <div key={todo.id}>
+          {todo.title}
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
-        Hello
+        <button
+          onClick={this.onButtonClick}
+        >Fetch</button>
+        {this.renderList()}
       </div>
     )
   }
